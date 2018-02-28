20c584530397fb7e879c6cadae3062c4ec1666bb
Jeetu singh <xtaraim@gmail.com>
2018-02-28 14:44:23 +0530

    Add files via upload
    Your First Machine Learning Project in Python Step-By-Step
    
    Do you want to do machine learning using Python, but you’re having trouble getting started?
    
    you will complete your first machine learning project using Python.
    
    
    
    Download and install Python SciPy and get the most useful package for machine learning in Python.
    Load a dataset and understand it’s structure using statistical summaries and data visualization.
    Create 6 machine learning models, pick the best and build confidence that the accuracy is reliable.
    If you are a machine learning beginner and looking to finally get started using Python, this projectl was designed for you

---
+++ Dataset.py
@@ -0,0 +1,68 @@
+
+
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+
+
+
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+
+
+# shape
+print(dataset.shape)
+
+
+# shape
+print(dataset.shape)
+
+
+
+
+# head
+print(dataset.head(20))
+
+
+
+# descriptions
+print(dataset.describe())
+
+# descriptions
+print(dataset.describe())
+
+
+
+# class distribution
+print(dataset.groupby('class').size())
+
+# class distribution
+print(dataset.groupby('class').size())
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
